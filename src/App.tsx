a3681cb06595426cdeb0cb859e1582fec702cd47
kadiebwe oscar <kadiebweoscar0@gmail.com>
2025-03-09 05:23:02 +0100

    fix: responsive for hero section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import SocialMedia from "./components/socialMedia";
 import whatsapp from "../src/assets/logo/Icônes de Whatsapp en téléchargement gratuit.jpeg";
 import gitHub from "../src/assets/logo/Download GitHub Logo, Git Hub Icon On White Background.jpeg";
 import linkdin from "../src/assets/logo/linkdinLogo.jpeg";
+import osCode from "../src/assets/logo/osCode.jpg";
 
 function openWhatsapp() {
   const phone: string = "243823616356";
@@ -58,34 +59,38 @@ const Typewriter: React.FC = () => {
 
 function App() {
   return (
-    <body className=" p-3 flex flex-col items-center flex flex-col items-center">
+    <body className="p-3 flex flex-col items-center">
       {/* hero section */}
-      <section className=" py-3 flex flex-col-reverse lg:flex-row lg:items-center lg:m-auto lg:w-[60%] lg:gap-3">
+      <section className="py-3 flex flex-col-reverse sm:flex-col-reverse md:flex-row lg:flex-row xl:flex-row 2xl:flex-row items-center m-auto w-full sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[60%] 2xl:w-[60%] gap-5">
         <div>
-          <div className=" py-4 flex gap-2">
-            <div className=" w-9 bg-black h-3.5"></div>
-            <p className=" text-4xl font-bold">Oscar Kadiebwe</p>
+          <div className="py-4 flex gap-2">
+            <div className="w-9 bg-black h-3.5"></div>
+            <p className="text-2xl sm:text-3xl md:text-4xl font-bold">
+              Oscar Kadiebwe
+            </p>
           </div>
-          <div className=" gap-2">
-            <div className=" flex gap-2">
-              <p className=" text-2xl">Je suis : </p>
+          <div className="gap-2">
+            <div className="flex gap-2">
+              <p className="text-xl sm:text-2xl">Je suis : </p>
               <Typewriter />
             </div>
-            <p className=" pl-2 py-3 text-justify border-l-2 border-l-sky-500">
+            <p className="pl-2 py-3 text-justify border-l-2 border-l-sky-500 text-sm sm:text-base md:text-lg">
               Passionné par la création d’applications performantes et
-              intuitives, j’aide entreprises et entrepreneurs à transformer
+              intuitives, j’aide les entreprises et entrepreneurs à transformer
               leurs idées en solutions digitales sur mesure.
             </p>
           </div>
         </div>
-        <div className=" flex flex-row w-full gap-2.5">
-          <div id="radiusImg">
-            <img src={whatsapp} alt="logo" id="radiusImg" />
-          </div>
-          <div
-            className=" h-[100%] flex flex-col justify-between gap-20 bg-blue-50
-          p-2.5 rounded-2xl"
-          >
+        <div className="flex flex-row w-full gap-2.5">
+          {/* <div id="radiusImg"> */}
+          <img
+            src={osCode}
+            alt="logo"
+            // id="radiusImg"
+            className="w-[80%] sm:w-[100%] md:w-[100%] rounded-3xl"
+          />
+          {/* </div> */}
+          <div className="h-[100%] flex flex-col justify-between gap-20 bg-blue-50 p-2.5 rounded-2xl">
             <SocialMedia
               urlImage={gitHub}
               altTxt="logo github"
@@ -105,32 +110,55 @@ function App() {
           </div>
         </div>
       </section>
-
-      {/* About me */}
-      {/* <div className=" flex flex-col items-center">
-        <div>
-          <img src={whatsapp} alt="logo" className=" rounded-2xl" />
-        </div>
-        <div>
-          <div>
-            <p className=" p-2.5">
-              Je suis un développeur spécialisé dans les applications et les
-              sites web. Mes compétences en développement web me permettent de
-              m'impliquer dans toutes les étapes du cycle de vie d'un projet, de
-              la conception à la mise en œuvre, et jusqu'à la maintenance.
-            </p>
-            <p className=" p-4 border-l-2 border-l-blue-400 text-2xl">
-              Donnez vie à vos projets numériques
-            </p>
-            <p className=" p-1">
-              Que ce soit pour un site vitrine, une application web interactive,
-              une application mobile ou un portail en ligne, je vous accompagne
-              dans la concrétisation de vos idées numériques.
-            </p>
-          </div>
-        </div>
-      </div> */}
     </body>
+    // <body className=" p-3 flex flex-col items-center flex flex-col items-center">
+    //   {/* hero section */}
+    //   <section className=" py-3 flex flex-col-reverse lg:flex-row lg:items-center lg:m-auto lg:w-[60%] lg:gap-3">
+    //     <div>
+    //       <div className=" py-4 flex gap-2">
+    //         <div className=" w-9 bg-black h-3.5"></div>
+    //         <p className=" text-4xl font-bold">Oscar Kadiebwe</p>
+    //       </div>
+    //       <div className=" gap-2">
+    //         <div className=" flex gap-2">
+    //           <p className=" text-2xl">Je suis : </p>
+    //           <Typewriter />
+    //         </div>
+    //         <p className=" pl-2 py-3 text-justify border-l-2 border-l-sky-500">
+    //           Passionné par la création d’applications performantes et
+    //           intuitives, j’aide les entreprises et entrepreneurs à transformer
+    //           leurs idées en solutions digitales sur mesure.
+    //         </p>
+    //       </div>
+    //     </div>
+    //     <div className=" flex flex-row w-full gap-2.5">
+    //       <div id="radiusImg">
+    //         <img src={whatsapp} alt="logo" id="radiusImg" />
+    //       </div>
+    //       <div
+    //         className=" h-[100%] flex flex-col justify-between gap-20 bg-blue-50
+    //       p-2.5 rounded-2xl"
+    //       >
+    //         <SocialMedia
+    //           urlImage={gitHub}
+    //           altTxt="logo github"
+    //           linkMedia="https://github.com/kadiebweoscar0"
+    //         />
+    //         <SocialMedia
+    //           urlImage={linkdin}
+    //           altTxt="logo facebook"
+    //           linkMedia="https://www.linkedin.com/login/fr"
+    //         />
+    //         <SocialMedia
+    //           urlImage={whatsapp}
+    //           altTxt="logo facebook"
+    //           linkMedia="#"
+    //           onClick={openWhatsapp}
+    //         />
+    //       </div>
+    //     </div>
+    //   </section>
+    // </body>
   );
 }
 
---
+++ src/components/frontEndCard.tsx
@@ -0,0 +1,79 @@
+import React from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import icon from "../assets/logo/Download GitHub Logo, Git Hub Icon On White Background.jpeg";
+
+const FrontEndCard: React.FC = () => {
+  return (
+    <div className="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/3 2xl:w-1/3 relative overflow-hidden bg-blue-50 shadow-lg rounded-xl p-3 sm:p-4 md:p-5 flex flex-col sm:flex-row items-center justify-between border transition-all duration-500 group">
+      {/* Icône */}
+      <div className="hidden sm:flex items-center justify-center w-16 h-16 sm:w-20 sm:h-20 md:w-24 md:h-24 m-2 sm:m-4 bg-gray-100 rounded-full z-10">
+        <img src={icon} alt="React" className=" rounded-full p-4 w-full" />
+      </div>
+
+      {/* Effet d'animation du bg */}
+      <div className="absolute inset-0 bg-gray-800 transform -translate-x-full transition-transform duration-500 group-hover:translate-x-0"></div>
+
+      {/* Contenu texte */}
+      <div className="z-10 mt-4 sm:mt-6 w-full sm:w-auto">
+        <div className="relative z-10 text-black group-hover:text-white transition-colors duration-500 flex flex-col gap-2 sm:gap-4">
+          <h2 className="text-xl sm:text-2xl md:text-3xl font-bold flex items-center">
+            ✅ Front-End
+          </h2>
+          <p className="text-gray-500 text-xs sm:text-sm group-hover:text-gray-300">
+            MA BOÎTE À OUTILS
+          </p>
+          <p className="text-gray-700 text-sm sm:text-base mt-1 group-hover:text-gray-300">
+            ReactJS, HTML, CSS, Sass, JavaScript, Bootstrap, Tailwind CSS,
+            Material UI
+          </p>
+        </div>
+
+        {/* Icônes des technologies en colonne (de bas en haut) */}
+        <div className="flex mt-2 justify-end gap-2">
+          <img
+            src={icon}
+            alt="React"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2"
+          />
+          <img
+            src={icon}
+            alt="HTML5"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2 -ml-5 sm:ml-02"
+          />
+          <img
+            src={icon}
+            alt="CSS3"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2 -ml-5 sm:ml-02"
+          />
+          <img
+            src={icon}
+            alt="Sass"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2 -ml-5 sm:ml-02"
+          />
+          <img
+            src={icon}
+            alt="JavaScript"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2 -ml-5 sm:ml-02"
+          />
+          <img
+            src={icon}
+            alt="Bootstrap"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2 -ml-5 sm:ml-02"
+          />
+          <img
+            src={icon}
+            alt="Tailwind CSS"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2 -ml-5 sm:ml-02"
+          />
+          <img
+            src={icon}
+            alt="Material UI"
+            className="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded-full border-sky-500 border-2 -ml-5 sm:ml-02"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FrontEndCard;
